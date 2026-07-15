92cf3d69c58acc690cb54f2640f9cb11ce97225c
emonhossen1672-cmd <emonhossen1672@gmail.com>
2026-07-15 09:24:44 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Home from './pages/Home.jsx'
 import QuestionBank from './pages/QuestionBank.jsx'
 import MCQ from './pages/MCQ.jsx'
 import Topics from './pages/Topics.jsx'
+import WrittenModelTestList from './pages/WrittenModelTestList.jsx'
+import WrittenModelTestExam from './pages/WrittenModelTestExam.jsx'
+
 import WrittenHub from './pages/WrittenHub.jsx'
 import WrittenExam from './pages/WrittenExam.jsx'
 import ExamPapers from './pages/ExamPapers.jsx'
@@ -22,6 +25,9 @@ export default function App() {
           <Route path="/mcq" element={<MCQ />} />
           <Route path="/topics" element={<Topics />} />
           <Route path="/written" element={<WrittenHub />} />
+          
+          <Route path="/written-model-tests" element={<WrittenModelTestList />} />
+<Route path="/written-model-tests/:id/exam" element={<WrittenModelTestExam />} />
           <Route path="/written/study" element={<WrittenExam />} />
           <Route path="/exam-papers" element={<ExamPapers />} />
           <Route path="/model-tests" element={<ModelTestList />} />
